f2f12a7541a512a7787903064feb497794edbca3
Sajith <krsajith@gmail.com>
2024-10-02 09:25:09 +0530

    Sample

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_widget_from_html/flutter_widget_from_html.dart';
+import 'package:http/http.dart' as http;
 
 void main() {
   runApp(const MyApp());
@@ -39,6 +41,7 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
 
+
   // This widget is the home page of your application. It is stateful, meaning
   // that it has a State object (defined below) that contains fields that affect
   // how it looks.
@@ -54,17 +57,27 @@ class MyHomePage extends StatefulWidget {
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
+Future<http.Response> fetchAlbum() {
+  return http.get(Uri.parse('https://b643-2406-8800-81-bae6-5ba-6f03-ae78-9d90.ngrok-free.app/81_Naveena_Upanyasangal-1.xhtml'));
+}
+
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
+  var htmlWidget = const HtmlWidget("first");
+  Future<void> _incrementCounter() async {
 
-  void _incrementCounter() {
-    setState(() {
+    var response = await fetchAlbum();
+
+    setState(()  {
+
       // This call to setState tells the Flutter framework that something has
       // changed in this State, which causes it to rerun the build method below
       // so that the display can reflect the updated values. If we changed
       // _counter without calling setState(), then the build method would not be
       // called again, and so nothing would appear to happen.
       _counter++;
+      htmlWidget =  HtmlWidget(response.body);
+
     });
   }
 
@@ -105,9 +118,7 @@ class _MyHomePageState extends State<MyHomePage> {
           // wireframe for each widget.
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
+        htmlWidget,
             Text(
               '$_counter',
               style: Theme.of(context).textTheme.headlineMedium,
